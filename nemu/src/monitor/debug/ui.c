812a57cbb9aa03dff634779c762c0cbe5474dd43
tracer-NEMU2020 <tjuics2020@163.com>
2021-11-08 11:31:56 +0000

    >  compile NEMU 2021229065 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:31:56 up  8:05,  1 user,  load average: 0.00, 0.01, 0.00 c029d176451703920f7693e1d7570842a66c38a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -54,10 +54,10 @@ static int cmd_info(char *args){ // 打印寄存器状态
 	if(arg == NULL || *arg == 'r'){
 		int i;
 		for(i = 0; i < 8; ++i)
-			printf("%s: 0x%x\t0x%x\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._16);
+			printf("%s: 0x%x 0x%x\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._16);
 		printf("\n");
 		for(i = 0; i < 4; i++)
-			printf("%s: 0x%x\t%s: 0x%x\n", regsb[i], cpu.gpr[i]._8[0], regsb[i+4], cpu.gpr[i]._8[1]);
+			printf("%s: 0x%x %s: 0x%x\n", regsb[i], cpu.gpr[i]._8[0], regsb[i+4], cpu.gpr[i]._8[1]);
 		
 		/*
 	 	printf("eax is %x\n",cpu.eax);
