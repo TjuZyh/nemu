54a345f6272745f2d0ca213bd309125afc5e08de
2021229065-Shi Yang <Shishishi996@163.com>
2021-11-08 04:41:35 +0000

    test

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,11 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+// TODO  test
+static int cmd_info(char *args){
+	return -1;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +51,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
+	{"info", "test",cmd_info},//TODO:  test
 
 	/* TODO: Add more commands */
 
