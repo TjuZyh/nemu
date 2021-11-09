efbfc5e0778a6972f14720acf87246d2dcabee1f
tracer-NEMU2020 <tjuics2020@163.com>
2021-11-09 02:58:10 +0000

    >  compile NEMU 2021229065 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:58:10 up 37 min,  1 user,  load average: 0.13, 0.07, 0.02 172ce3b44cbcc6fc2c944ae229e3e956387dc182

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,4 +1,3 @@
-/*
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
@@ -18,9 +17,8 @@ void init_wp_pool() {
 	head = NULL;
 	free_ = wp_pool;
 }
-*/
+
 /* TODO: Implement the functionality of watchpoint */
-/*
 WP *new_wp(char *args){ // 新增一个监视点
 	if(!free_)
 		assert(0);
@@ -50,6 +48,7 @@ WP *new_wp(char *args){ // 新增一个监视点
 void free_wp(WP* wp){ // 删除一个监视点
 	if(wp == 0)
 		return;
+	wp->using = 0;
 	if(head == wp){ // wp是第一个监视点
 		head = wp -> next;
 		wp -> next = 0;
@@ -105,105 +104,4 @@ void info_w(){ // 打印监视点
 		if(wp_pool[i].using) 
 			printf("%2d\t%c\t0x%x\t%s\n", wp_pool[i].no, wp_pool[i].type, wp_pool[i].value, wp_pool[i].expression);
 	}
-}
-*/
-
-#include "monitor/watchpoint.h"
-#include "monitor/expr.h"
-
-#define NR_WP 32
-
-static WP wp_pool[NR_WP];
-static WP *head, *free_;
-
-void init_wp_pool() {
-	int i;
-	for(i = 0; i < NR_WP; i ++) {
-		wp_pool[i].no = i;
-		wp_pool[i].next = &wp_pool[i + 1];
-	}
-	wp_pool[NR_WP - 1].next = NULL;
-
-	head = NULL;
-	free_ = wp_pool;
-}
-
-/* TODO: Implement the functionality of watchpoint */
-
-WP* new_wp(char *str_){
-	if(!free_) assert(0);
-
-	WP* now_wp = free_;		//第一个空闲wp
-	free_ = free_->next;
-
-	now_wp->type = 'w'; now_wp->using = 1;
-	strcpy(now_wp->expression, str_);
-	bool success; now_wp->value = expr(str_,&success);
-	now_wp->next = NULL;
-
-	if(!head){				//head为空
-		head = now_wp;
-	}
-	else {
-		WP *qwq = head;
-		while (qwq->next) {	//qwq是上一个监视点
-			qwq = qwq->next;
-		}
-		qwq->next = now_wp;
-	}
-	printf("Number of watch is : %d\n", now_wp->no);
-	return now_wp;
-}
-
-void free_wp(WP* wp){
-	if(wp == 0) return ;
-	wp->using = 0;
-	if(head == wp){			//wp 是 第一个监视点
-		head = wp->next; wp->next = 0;
-
-		if(!free_) free_ = wp;
-		else {
-			WP* qwq = free_;
-			while (qwq->next) qwq = qwq->next;		//qwq是当前最后一个free_的wp
-			qwq->next = wp;
-		}
-	}
-	else {
-		WP *qwq = head;
-		while (qwq->next != wp) qwq = qwq->next; 	//qwq->next == wp
-		qwq->next = wp->next; wp->next = 0;
-
-		if(!free_) free_ = wp;
-		else {
-			qwq = free_;
-			while (qwq->next) qwq = qwq->next;		//qwq是当前最后一个free_的wp
-			qwq->next = wp;
-		}
-	}
-}
-
-int change(){				//判断value是否变化
-
-	WP *i = head; int num=0;//num为变了的个数
-	bool success;
-	while(i){
-		i->new_value = expr(i->expression, &success);
-		if(i->new_value != i->value) num++;
-		i = i->next;
-	}
-	return num;
-}
-
-void info_w(){
-	printf("Num\tType\tValue\tWhat\n");
-	int i;
-	for(i=0; i < NR_WP; i++){
-		if(wp_pool[i].using) printf("%2d\t%c\t0x%x\t%s\n", wp_pool[i].no, wp_pool[i].type, wp_pool[i].value, wp_pool[i].expression);
-	}
-}
-
-WP* find_n(int n){
-	if((n < 0) || (NR_WP <= n)) {printf("this wp was not existed\n"); return 0;}
-	if(wp_pool[n].using == 0) {printf("this wp was not used\n"); return 0;}
-	return &wp_pool[n];
 }
