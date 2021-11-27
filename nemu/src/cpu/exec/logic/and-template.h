3e8324aebbf4c07882eed6df960b311486358fe1
tracer-NEMU2020 <tjuics2020@163.com>
2021-11-27 16:35:53 +0000

    >  compile NEMU 2021229065 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:35:53 up  1:45,  1 user,  load average: 0.04, 0.06, 0.01 dd71ddcbea1892723e88f0bfdde1a16a7d4bfabd

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,13 +3,19 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & op_src->val;
-	OPERAND_W(op_dest, result);
+	DATA_TYPE ret = op_dest->val & op_src->val;
+	OPERAND_W(op_dest, ret);
 
-        update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-        cpu.eflags.CF = cpu.eflags.OF = 0;
-
-
+	/* TODO: Update EFLAGS. */
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	cpu.eflags.ZF = !ret;
+    cpu.eflags.SF = ret >> ((DATA_BYTE << 3) - 1);
+	ret ^= ret >> 4;
+    ret ^= ret >> 2;
+    ret ^= ret >> 1;
+    ret &= 1;
+    cpu.eflags.PF = !ret;
 	print_asm_template2();
 }
 
