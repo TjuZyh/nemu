ca25a05954a2941c786f7cb2c6e4eebe6d1693c3
tracer-NEMU2020 <tjuics2020@163.com>
2021-11-29 14:16:17 +0000

    >  compile NEMU 2021229065 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:16:17 up 11:32,  1 user,  load average: 0.49, 0.32, 0.21 89ae732d22e527c1be4803711e23c1c49914c61

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,21 +1,8 @@
 #include "FLOAT.h"
-#include <stdint.h>
-
-typedef union{
-	struct{
-		uint32_t m : 23;
-		uint32_t e : 8;
-		uint32_t s : 1;
-	};
-
-	uint32_t val;
-}Float;
-
-#define _sign(x) ((x) & 0x80000000)
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	int64_t scale = ((int64_t)a * (int64_t)b) >> 16;
-	return scale;
+	long long ret = 1ll * a * b;
+	return (FLOAT)(ret >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -36,12 +23,22 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * It is OK not to use the template above, but you should figure
 	 * out another way to perform the division.
 	 */
+	int op = 1;
+	if ((a < 0) == 1) op *= -1,a = -a;
+	if ((b < 0) == 1) op *= -1,b = -b;
 
-	FLOAT q, r;
-	__asm__ __volatile__("idiv %2" 
-							: "=a"(q), "=d"(r) 
-							: "r"(b), "a"(a << 16), "d"(a >> 16));
-	return q;
+	int ret = a / b;
+	a %= b;
+
+	int i;
+	for (i = 0;i < 16;i ++){
+		a <<= 1;
+		ret <<= 1;
+		if (a >= b) a -= b, ret |= 1;
+	}
+	return op * ret; 
+
+	
 }
 
 FLOAT f2F(float a) {
@@ -55,18 +52,28 @@ FLOAT f2F(float a) {
 	 * performing arithmetic operations on it directly?
 	 */
 
-	Float f;
-	void *temp = &a;
-	f.val = *(uint32_t *)temp;
-	uint32_t m = f.m | (1 << 23);
-	int shift = 134 - (int)f.e;
-	if(shift < 0) m <<= (-shift);
-	else m >>= shift;
-	return (_sign(f.val) ? -m : m);
+	int b = *(int *)&a;
+	int sign = b & 0x80000000;
+	int exp = (b >> 23) & 0xff;
+	int last = b & 0x7fffff;	
+	
+	if(exp == 255) {
+		if (sign) return -0x7fffffff;
+		else return 0x7fffffff;
+	}
+	
+	if(exp == 0) return 0;
+
+	last |= 1 << 23;
+	exp -= 134;	
+	if (exp < 0) last >>= -exp;
+	if (exp > 0) last <<= exp;
+
+	if (sign) return -last;else return last;
 }
 
 FLOAT Fabs(FLOAT a) {
-	return _sign(a) ? -(a) : (a);
+	if (a < 0) return -a;else return a;
 }
 
 /* Functions below are already implemented */
